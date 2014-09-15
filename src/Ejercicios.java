d2c87d789c384b0939a99ee9efd05e0e5101d4dd
Turupawn <turupawn@userr>
2014-09-14 21:48:04 -0600

    test

---
+++ src/Ejercicios.java
@@ -0,0 +1,41 @@
+
+public class Ejercicios {
+
+	//Realizar la siguiente funcion recursiva
+	//ejercicio1(n) = ejercicio1(n-1) + ejercicio1(n-2)
+	//Dados los siguientes caso base
+	//ejercicio1(0)=0
+	//ejercicio1(1)=1
+	static int ejercicio1(int n)
+	{
+		return -1; 
+	}
+	
+	
+	//Realizar la siguiente funcion recursiva
+	//ejercicio2(n) = ejercicio2(n-1) - ejercicio2(n-2)
+	//Dados los siguientes caso base
+	//ejercicio2(0)=2
+	//ejercicio2(1)=3
+	static int ejercicio2(int n)
+	{
+		return -1; 
+	}
+	
+	//Realizar la siguiente funcion recursiva
+	//ejercicio3(n) = ejercicio3(n-1) * ejercicio3(n-2) - ejercicio3(n-2) + 12
+	//Dados los siguientes caso base
+	//ejercicio3(0)=0
+	//ejercicio3(1)=1
+	//ejercicio3(2)=2
+	static int ejercicio3(int n)
+	{
+		return -1; 
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
